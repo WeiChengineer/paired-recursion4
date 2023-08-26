cdf6b3e9c1f09c5b1e11b07bb7b28d3be87f4cf7
Wei Cheng Chen <DrChenWeiCheng@gmail.com>
2023-08-26 10:27:31 -0700

    problem 15

--- problems/15-bonus-subsets.js
+++ problems/15-bonus-subsets.js
@@ -5,10 +5,7 @@ Write a function called `subsets` that will return all subsets of an array.
 
 Examples: 
 
-subsets([]) // [[]]
-subsets([1]) // [[], [1]]
-subsets([1, 2]) // [[], [1], [2], [1, 2]]
-subsets([1, 2, 3]) // [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+
 
 Hint: For subsets([1, 2, 3]), there are two kinds of subsets:
   1. Those that do not contain 3 (all of these are subsets of [1, 2]).
@@ -17,9 +14,20 @@ Hint: For subsets([1, 2, 3]), there are two kinds of subsets:
 ***********************************************************************/
 
 const subsets = (arr) => {
-  // Your code here 
-}
+  if (arr.length === 0) {
+    return [[]]
+  }
+  let lastEl = arr[arr.length - 1]
+  let withoutEl = arr.slice(0, arr.length - 1)
+  let smallerSub = subsets(withoutEl);
+  let withEl = smallerSub.map(subset => [...subset, lastEl]);
+  return [...smallerSub, ...withEl]
 
+}
+console.log(subsets([1])) // [[], [1]]
+console.log(subsets([1, 2])) // [[], [1], [2], [1, 2]]
+console.log(subsets([1, 2, 3])) // [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+console.log(subsets([])) // [[]]
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = subsets;
